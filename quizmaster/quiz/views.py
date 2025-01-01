bc23b0178919d97df7f66bff2a486c3a56957b0e
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-01 08:19:10 +0530

    init

--- quizmaster/quiz/views.py
+++ quizmaster/quiz/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from rest_framework import api_view
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
 # Create your views here.
+
+@api_view(["POST"])
+def register(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response({"error": "Username and password required"}, status=status.HTTP_400_BAD_REQUEST)
+    elif User.objects.filter(username=username).exists():
+        return Response({"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+    user=User.objects.create_user(username=username, password=password)
+    return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    user= authenticate(username=username, password=password)
+    
